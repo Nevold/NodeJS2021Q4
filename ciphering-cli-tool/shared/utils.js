330399fbbff47ddfaec5f0258e0ca3e522d456e1
Nevold <nevold@tut.by>
2021-11-14 23:09:08 +0300

    feat: add duplicate function

--- ciphering-cli-tool/shared/utils.js
+++ ciphering-cli-tool/shared/utils.js
@@ -8,4 +8,14 @@ outputFile = () => {
   return argv[argv.indexOf('-o') + 1];
 };
 
-module.exports = { inputFile, outputFile };
+isDuplicate = () => {
+  return (
+    [
+      argv.filter((item) => item == '-c').length,
+      argv.filter((item) => item == '-i').length,
+      argv.filter((item) => item == '-o').length,
+    ].filter((item) => item > 1).length > 0
+  );
+};
+
+module.exports = { inputFile, outputFile, isDuplicate };
